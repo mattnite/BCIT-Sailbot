a1db5a1cc69f4befbd7525ada3533958b06c16bd
Matthew Knight <mgk1795@gmail.com>
2016-11-06 15:24:29 -0800

    Made node structure and initialized ncurses

--- pathfinding/aSharp.cc
+++ pathfinding/aSharp.cc
@@ -3,3 +3,34 @@
 // Author: Matthew Knight
 // Date: 2016-11-06
 
+// This program demonstrates the A* pathfinding algorithm on a 20x20 mesh.
+// There is no weighting, the mesh will "play" and will show the steps of how
+// the algroithm found the destination. Start, destination, and obstacles will
+// be randomly generated.
+
+#include <ncurses.h>
+#include "meshNode.h"
+
+typedef struct Node
+{
+    Node* next;
+    Node* last;
+    int x;
+    int y;
+    int cost;
+}
+
+static int n = 20;			// Length and width of mesh
+
+int main()
+{
+    initscr();				// Start curses mode
+    
+    // Generate End node
+
+    // Generate Beginning node, make sure it isn't the end node
+    
+    endwin();				// End window
+
+    return 0;
+}
