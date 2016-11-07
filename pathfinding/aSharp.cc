fbb3a4ecdc06cb96727dfbe847d1dac6d3172a33
Matthew Knight <mgk1795@gmail.com>
2016-11-06 17:07:51 -0800

    Added freeList function which deletes the nodes in the linked lists

--- pathfinding/aSharp.cc
+++ pathfinding/aSharp.cc
@@ -8,27 +8,63 @@
 // the algroithm found the destination. Start, destination, and obstacles will
 // be randomly generated.
 
-#include <ncurses.h>
-#include "meshNode.h"
-
-typedef struct Node
-{
-    Node* next;
-    Node* last;
-    int x;
-    int y;
-    int cost;
-}
+#include <ncurses.h>			// For the ncurses environment
+#include <time.h>
+#include <stdlib.h>			// rand()
 
 static int n = 20;			// Length and width of mesh
 
+typedef struct Node
+{
+    node* next;				// Next node in list
+    node* last;				// Last node in A* algorithm
+    int x;				// X position of node
+    int y;				// Y position of node
+    int cost;				// Cost of node
+} node;
+
+// Function for deallocating memory taken up by the node objects in list
+int freeList(node* list)
+{
+    while(list != NULL)
+    {
+	next = list*;
+	free(list);
+	list = next;
+    }
+
+    return 0;
+}
+
 int main()
 {
-    initscr();				// Start curses mode
+    srand(time(NULL));			// Time seed for random generator
     
-    // Generate End node
+    node* prospected = 0;		// initialize prospected list
+    nose* obstacles = 0;		// initialize obstacles
+    
+    initscr();				// Start curses mode
+
+
+    // Generate End coordinates
+    endX = rand() % n;
+    endY = rand() % n;
 
     // Generate Beginning node, make sure it isn't the end node
+    node* visited = (node*)malloc(sizeof(Node)); 
+    
+    // Add Beginning node to the prospected list
+	/* Begin pathfinding algorithm:
+	    1. 
+	    2. 
+	    3. 
+	    4.  
+	*/
+    
+    // deallocate memory of all the nodes
+    freeList(visited);
+    freeList(prospected);
+    freeList(obstacles);
     
     endwin();				// End window
 
