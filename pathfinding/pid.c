19a5c8851c93483c505b69d1d96c12e40b89d093
Matthew Knight <mgk1795@gmail.com>
2016-12-24 16:43:03 -0800

    Started PID Control loops

---
+++ pathfinding/pid.c
@@ -0,0 +1,8 @@
+// PID Controller Implementation File
+
+// Author: Matthew Knight
+// Date: 2016-12-22
+
+// This module allows for the implemenation of PID control loops
+
+
--- pathfinding/pid.h
+++ pathfinding/pid.h
@@ -1,8 +1,10 @@
-// PID Controller interface
+// PID Controller interface File
 
 // Author: Matthew Knight
 // Date: 2016-12-21
 
-// This module allows for the implemenation of PID Control 
+// This module allows for the implemenation of PID Control loops.
+
+
 
 
