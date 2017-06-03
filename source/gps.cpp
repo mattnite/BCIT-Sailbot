97fc18c704f2f79ce6c962345848cd022da49f5a
Matthew Knight <mgk1795@gmail.com>
2017-06-03 01:03:26 +0000

    started on gps

---
+++ source/gps.cpp
@@ -0,0 +1,22 @@
+// GPS Server
+
+// Author: Matthew Knight
+// FileName: gps.cpp
+// Date: 2017-06-02
+
+// This program is the gps server for the sailbot. If a program requests the gps
+// coordinates it will reply with the last sampled position.
+
+#include <iostream>
+#include <atomic>
+#include <thread>
+#include <chrono>
+#include "zmq.hpp"
+
+// Sampling thread
+
+// Main thread (server thread)
+int main(void)
+{
+
+}
