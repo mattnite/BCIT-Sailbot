41795018a156c73c669139151ce19497cb24d8ac
Matthew Knight <mgk1795@gmail.com>
2017-06-05 19:21:48 -0700

    working on gps thread

--- source/gps.cpp
+++ source/gps.cpp
@@ -1,29 +1,32 @@
-// GPS Server
+// GPS Thread
 
 // Author: Matthew Knight
 // FileName: gps.cpp
 // Date: 2017-06-02
 
-// This program is the gps server for the sailbot. If a program requests the gps
-// coordinates it will reply with the last sampled position.
+// This is the gps thread for the sailbot. It periodically updates the gps
+// coordinates in the system variables table.
 
 #include <iostream>
 #include <atomic>
 #include <thread>
 #include <chrono>
-#include "zmq.hpp"
+#include <ctime>
 
 // Sampling thread
 void gps(void)
 {
-    std::chrono::duration interval(milliseconds(100));
+    std::chrono::duration<int, std::milli> interval;
+    std::chrono::system_clock::time_point current;
+    
     while (true)
     {
-	std::chrono::time_point current = std::chrono::system_clock::now();
+	current = std::chrono::system_clock::now();
 	
 	// Do things
+	std::cout << "Reading GPS at " << std::endl;
 
-	if (current + interval > std::chrono::system_clock::now();)
+	if (current + interval > std::chrono::system_clock::now())
 	    std::this_thread::sleep_until(current+interval);
     }
 }
@@ -32,6 +35,7 @@ void gps(void)
 int main(void)
 {
     // create sampling thread
+
     std::thread (gps, NULL).detach();
 
     // run forever
