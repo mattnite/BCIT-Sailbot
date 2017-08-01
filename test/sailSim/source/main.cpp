88acf1e068659a7dfbbe0023d09b4a784cac84b0
Matthew Knight <mgk1795@gmail.com>
2017-08-01 08:50:13 -0700

    added key control of windspeed and aileron

--- test/sailSim/source/main.cpp
+++ test/sailSim/source/main.cpp
@@ -12,7 +12,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <cmath>
-
+#include <iostream>
 #include "sailEntities.hpp"
 
 int main()
@@ -25,24 +25,67 @@ int main()
     window.setVerticalSyncEnabled(true);
 
     float time = 0;
+
+    float angle = 0, speed = 10;
     wingSail sailbot(240, 320, 0, 30);
+    arrow wind(speed, 2, 90, 240, 50, sf::Color::White);
 
     while (window.isOpen())
     {
 	sf::Event event;
 	while (window.pollEvent(event))
 	{
-	    if (event.type == sf::Event::Closed)
-		window.close();
+	    switch (event.type)
+	    {
+		case sf::Event::Closed:
+		    window.close();
+		    break;
+		case sf::Event::KeyPressed:
+		    switch (event.key.code)
+		    {
+			case sf::Keyboard::Right:
+			    angle += 1;
+			    break;
+			case sf::Keyboard::Left:
+			    angle -= 1;
+			    break;
+			case sf::Keyboard::Up:
+			    speed += 1;
+			    break;
+			case sf::Keyboard::Down:
+			    speed -= 1;
+			    break;
+			default:
+			    break;
+		    };
+
+		    break;
+		default:
+		    break;
+	    };
+	
 	}
 	
 	sf::Time elapsed = clock.restart();
 	time += elapsed.asSeconds();
 	
-	sailbot.setState(120*time, 200*time);
+
+	if (angle > 20)
+	    angle = 20;
+	if (angle < -20)
+	    angle = -20;
+
+	if (speed > 50)
+	    speed = 50;
+	if (speed < 0)
+	    speed = 0;
+	
+	sailbot.setState(90, angle);
+	wind.setPolar(speed, 90);
 
 	window.clear(sf::Color::Black);
 	window.draw(sailbot);
+	window.draw(wind);
 	window.display();
 
 	sf::sleep(sf::milliseconds(33));
